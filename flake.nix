47ccead7e11eab95c5a8ffc64158d8ab0ff09b78
Ulugbek Raxmankulov <mylogin.ulugbek@gmail.com>
2025-09-28 14:53:12 +0500

    init nix

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Enhanced NixOS configuration (flake) for acer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = {
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    ...
+  }:
+  {
+      nixosConfigurations = {
+        msi = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inputs = {
+              inherit
+                nixpkgs
+                nixpkgs-unstable
+                home-manager
+                ;
+            };
+            inherit
+              nixpkgs
+              nixpkgs-unstable
+              home-manager
+              ;
+          };
+          modules = [
+            ./hosts/msi/configuration.nix
+          ];
+        };
+      };
+  };
+
+}
